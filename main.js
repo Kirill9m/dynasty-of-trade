60c128b04d9aa495400048464b07e0af5e389cf6
Kirill <kirillsavorin@gmail.com>
2024-11-02 00:35:54 +0100

    Dom manipulation, img can be added with addTree();

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-    const mainGameWindow = document.querySelector("#main-game-window");
+const mainGameWindow = document.querySelector("#main-game-window");
     const columns = 20;
     const rows = 10;
 
@@ -11,19 +9,23 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let row = 1; row <= rows; row++) {
         for (let col = 1; col <= columns; col++) {
             const cell = document.createElement("div");
-            cell.classList.add(`${row}x${col}`);
+            cell.id = (`c${row}r${col}`);
             cell.classList.add("grid-cell");
             mainGameWindow.appendChild(cell);
         }
     }
-});
 
+    function getRandomInt(max) {
+       return Math.floor(Math.random() * max) + 1;
+    }
 
+    const tree = new Image(32, 32);
+    tree.src = "/img/tree.png";
 
-function getRandomInt(max) {
-    Math.floor(Math.random() * max) + 1;
-}
-
-function addTree(tree) {
-
-}
+    function addTree() {
+        const row = getRandomInt(rows);
+        const col = getRandomInt(columns);
+        rand = (`#c${row}r${col}`);
+        console.log(rand);
+        document.querySelector(rand).appendChild(tree);
+    }
