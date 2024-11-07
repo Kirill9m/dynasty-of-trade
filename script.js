66424020d8caef2f2e9bdbc388ddea5166c15355
Kirill <kirillsavorin@gmail.com>
2024-11-07 22:16:16 +0100

    Chose state menu, new object for dog states

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let playerState = 'sit';
+const dropdown = document.getElementById('animations');
+dropdown.addEventListener('change', function(e) {
+    playerState = e.target.value;
+});
+
 const canvas = document.getElementById("main-game-window");
 const ctx = canvas.getContext("2d");
 const CANVAS_HEIGHT = canvas.height = 600;
@@ -9,30 +15,82 @@ const spriteWidth = 575;
 // 6876px width / 12
 const spriteHeight = 523;
 // 5230px / 10
-let frameX = 0;
-let frameY = 5;
 let gameFrame = 0;
-let staggerFrame = 15;
+let staggerFrame = 3;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7
+    },
+    {
+        name: 'fall',
+        frames: 7
+    },
+    {
+        name: 'run',
+        frames: 9
+    },
+    {
+        name: 'dizzy',
+        frames: 11
+    },
+    {
+        name: 'sit',
+        frames: 5
+    },
+    {
+        name: 'roll',
+        frames: 7
+    },
+    {
+        name: 'bite',
+        frames: 7
+    },
+    {
+        name: 'ko',
+        frames: 12
+    },
+    {
+        name: 'getHit',
+        frames: 4
+    }
+];
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+    for(let j = 0; j < state.frames; j++){
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
+    }
+    spriteAnimations[state.name] = frames;
+});
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrame) % 6;
+    let position = Math.floor(gameFrame/staggerFrame) % spriteAnimations[playerState].loc.length;
     frameX = spriteWidth * position;
-    ctx.drawImage(playerImg, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    let frameY = spriteAnimations[playerState].loc[position].y;
+    ctx.drawImage(playerImg, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
     gameFrame++;
     requestAnimationFrame(animate);
 };
 
 const move = document.querySelector(".move_btn");
 move.addEventListener("click", (event) => {
-    frameY++;
+    playerState = 'idle';
 });
 
 const speed = document.querySelector(".shoot_btn");
 speed.addEventListener("click", (event) => {
-    staggerFrame++;
-    if(staggerFrame > 5)
-        staggerFrame = 0;
+    if(staggerFrame < 8)
+        staggerFrame++;
 });
 
 animate();
