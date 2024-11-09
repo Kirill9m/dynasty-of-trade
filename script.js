918290bc43717de793baac3a734e8f3b78a9fded
Kirill <kirillsavorin@gmail.com>
2024-11-09 15:13:53 +0100

    New class added + constructor applied for images and speed modifier

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let playerState = 'sit';
 const dropdown = document.getElementById('animations');
-dropdown.addEventListener('change', function(e) {
+dropdown.addEventListener('change', function (e) {
     playerState = e.target.value;
 });
 
@@ -21,6 +21,7 @@ const backgroundLayer4 = new Image();
 backgroundLayer4.src = 'img/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'img/layer-5.png';
+
 const spriteWidth = 575;
 // 6876px width / 12
 const spriteHeight = 523;
@@ -29,6 +30,43 @@ let gameFrame = 0;
 let staggerFrame = 3;
 let gameSpeed = 5;
 const spriteAnimations = [];
+
+class Layer {
+    constructor(image, speedModifier) {
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.x2 = this.width;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+    }
+    update() {
+        this.speed = gameSpeed * this.speedModifier;
+        if (this.x <= -this.width) {
+            this.x = this.width + this.x2 - this.speed;
+        }
+        if (this.x2 <= -this.width) {
+            this.x2 = this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
+    }
+    draw() {
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
+}
+
+const layer1 = new Layer(backgroundLayer1, 0.5);
+const layer2 = new Layer(backgroundLayer2, 0.1);
+const layer3 = new Layer(backgroundLayer3, 0.1);
+const layer4 = new Layer(backgroundLayer4, 0.6);
+const layer5 = new Layer(backgroundLayer5, 0.8);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+
 const animationStates = [
     {
         name: 'idle',
@@ -75,26 +113,30 @@ animationStates.forEach((state, index) => {
     let frames = {
         loc: [],
     }
-    for(let j = 0; j < state.frames; j++){
+    for (let j = 0; j < state.frames; j++) {
         let positionX = j * spriteWidth;
         let positionY = index * spriteHeight;
-        frames.loc.push({x: positionX, y: positionY});
+        frames.loc.push({ x: positionX, y: positionY });
     }
     spriteAnimations[state.name] = frames;
 });
 
-let x = 0;
-let x2 = 2400;
-
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4, x, 0);
-    ctx.drawImage(backgroundLayer4, x2, 0);
-    if(x < -2400) x = 2400 + x2 - gameSpeed;
-    else x -= gameSpeed;
-    if(x2 < -2400) x2 = 2400 + x - gameSpeed;
-    else x2 -= gameSpeed;
-    let position = Math.floor(gameFrame/staggerFrame) % spriteAnimations[playerState].loc.length;
+    layer1.draw();
+    layer1.update();
+    layer2.draw();
+    layer2.update();
+    layer3.draw();
+    layer3.update();
+    layer4.draw();
+    layer4.update();
+    layer5.draw();
+    layer5.update();
+    gameObjects.forEach(object => {
+
+    });
+    let position = Math.floor(gameFrame / staggerFrame) % spriteAnimations[playerState].loc.length;
     frameX = spriteWidth * position;
     let frameY = spriteAnimations[playerState].loc[position].y;
     ctx.drawImage(playerImg, frameX, frameY, spriteWidth, spriteHeight, -30, 100, spriteWidth, spriteHeight);
@@ -109,7 +151,7 @@ move.addEventListener("click", (event) => {
 
 const speed = document.querySelector(".shoot_btn");
 speed.addEventListener("click", (event) => {
-    if(staggerFrame < 8)
+    if (staggerFrame < 8)
         staggerFrame++;
 });
 
