63c44befdf66033adddb7da768627cbdd9508256
Kirill <kirillsavorin@gmail.com>
2024-11-09 21:57:44 +0100

    Enemy animation firstOne

--- gameClasses.js
+++ gameClasses.js
@@ -35,19 +35,19 @@ export class Enemy {
     constructor() {
         this.image = new Image();
         this.image.src = 'img/enemy1.png';
-        this.x = Math.random()*canvas.width;
-        this.y = Math.random()*canvas.height;
-        this.speed = Math.random() * 4 - 2;
+        // this.speed = Math.random() * 4 - 2;
         this.spriteWidth = 293;
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 4;
         this.height = this.spriteHeight / 4;
+        this.x = Math.random()*(canvas.width - this.width);
+        this.y = Math.random()*(canvas.height - this.height - 120);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update() {
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += Math.random() * 5 -2.5;
+        this.y += Math.random() * 5 - 2.5;
         if (gameFrame % this.flapSpeed === 0){
             this.frame > 4 ? this.frame = 0 : this.frame++;
         }
